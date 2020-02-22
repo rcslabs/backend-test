92095fdc1a3b38d462ed9c5e6a6a43785e5f6562
omcode_dev_3 <permanovd@gmail.com>
2020-02-22 19:26:20 +0300

    #1: Initial commit. Add framework files.

---
+++ src/main/kotlin/com/permanovd/taxes_management/TaxesManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.permanovd.taxes_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxesManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaxesManagementApplication>(*args)
+}
