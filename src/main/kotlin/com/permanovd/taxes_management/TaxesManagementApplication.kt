9ab20fa9093144e80ed0304965f507870206bf49
omcode_dev_3 <permanovd@gmail.com>
2020-02-25 22:38:53 +0300

    #2: Add data retrieval repo method. Add api method.
    Add cors handling. Move data source connection to props from
    configuration class. Add readme file.

--- src/main/kotlin/com/permanovd/taxes_management/TaxesManagementApplication.kt
+++ src/main/kotlin/com/permanovd/taxes_management/TaxesManagementApplication.kt
@@ -1,9 +1,11 @@
 package com.permanovd.taxes_management
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@MapperScan("com.permanovd.taxes_management.infrastructure")
 class TaxesManagementApplication
 
 fun main(args: Array<String>) {
