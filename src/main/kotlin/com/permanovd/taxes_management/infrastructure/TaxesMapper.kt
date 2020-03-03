198d3f8d6ccb07763692d950234312d9bc62835a
omcode_dev_3 <permanovd@gmail.com>
2020-03-03 23:33:23 +0300

    #3: Fix sql query. Fix jar name in docker entrypoint and maven config.

--- src/main/kotlin/com/permanovd/taxes_management/infrastructure/TaxesMapper.kt
+++ src/main/kotlin/com/permanovd/taxes_management/infrastructure/TaxesMapper.kt
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.ResultType
 import org.apache.ibatis.annotations.Select
 
 public interface TaxesMapper {
-    @Select("SELECT \${row} as \${rowAlias}, \${column} as \${columnAlias}, v as value FROM source_data GROUP BY \${row}, \${column}")
+    @Select("SELECT \${row} as \${rowAlias}, \${column} as \${columnAlias}, SUM(v) as value FROM source_data GROUP BY \${row}, \${column}")
     @ResultType(List::class)
     fun allAggregated(@Param("column") column: String,
                       @Param("row") row: String,
